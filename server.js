5db82033d83340d40ba4556de4fe01d66a42b14e
William Madden <madden.william.d@gmail.com>
2019-07-24 14:28:50 -0700

    added books field to authors query that returns a list of books which have an author ID that matches the ID of each author returned

--- server.js
+++ server.js
@@ -47,10 +47,16 @@ const AuthorType =  new GraphQLObjectType({
     description:  "This represents an author of a book",
     fields: () => ({
         id: {type: GraphQLNonNull(GraphQLInt)} ,
-        name: { type: GraphQLNonNull(GraphQLString)}
+        name: { type: GraphQLNonNull(GraphQLString)},
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: (author) => {
+                return books.filter(book => book.authorId === author.id)
+            }
+        }
     })
 });
- 
+
 const RootQueryType = new GraphQLObjectType({
     name: "query",
     description: "Root Query",
